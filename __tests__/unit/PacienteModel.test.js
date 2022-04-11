35e818176c4483863518e75d1b02f32556984234
Nayara Souza <nayara.souza@ccc.ufcg.edu.br>
2022-04-11 19:56:02 -0300

    Adicionado Teste Unitário para PacienteModel
    Criado testes utilizando Jest para  PacienteModel para validação do seu uso no código.
    
    Signed-off-by: Nayara Souza <nayara.souza@ccc.ufcg.edu.br>

---
+++ __tests__/unit/PacienteModel.test.js
@@ -0,0 +1,24 @@
+const PacienteModel = require("../../src/models/PacienteModel");
+
+describe('Testar o Objeto Paciente', () => {
+    test('Criar o Paciente corretamente', () => {
+        const antonio = new PacienteModel("Antonio", "13/01/1998", "22/05/2022", "12:00")
+
+        const pacienteAntonio =
+            {nome: 'Antonio',
+                dataNascimento: '13/01/1998',
+                dataAgendada: '22/05/2022',
+                horaAgendada: '12:00',
+                "status": false};
+
+        expect(antonio).toEqual(pacienteAntonio)
+    });
+
+    test('Mudar o status do Paciente corretamente', () => {
+        const antonio = new PacienteModel("Antonio", "13/01/1998", "22/05/2022", "12:00")
+
+        antonio.setStatus(true)
+        expect(antonio.status).toEqual(true);
+    });
+
+});
