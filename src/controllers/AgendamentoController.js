599b56f12c6b34e32f618b7ee16332a25b6ee648
Nayara Souza <nayara.souza@ccc.ufcg.edu.br>
2022-04-12 01:49:12 -0300

    Implementado o método agendarPaciente
    Foi implementado o método que adiciona os agendamentos dos Pacientes em um Map, em que a sua key é a data do agendamento.
    
    Signed-off-by: Nayara Souza <nayara.souza@ccc.ufcg.edu.br>

--- src/controllers/AgendamentoController.js
+++ src/controllers/AgendamentoController.js
@@ -4,9 +4,26 @@ module.exports = class AgendamentoController {
 
     agendamentos = new Map();
 
-
     async agendarPaciente(request, response) {
-        // TODO - Método de agendar paciente
+        const { nome, dataNascimento, dataAgendada, horaAgendada } = request.body;
+
+        const paciente = new PacienteModel(nome, dataNascimento, dataAgendada, horaAgendada);
+
+        const pacientesPorDataAgendada = this.#getPacientesPorDataAgendada(dataAgendada);
+
+        pacientesPorDataAgendada.push(paciente);
+
+        this.agendamentos.set(dataAgendada, pacientesPorDataAgendada);
+        console.log(this.agendamentos)
+
+        response.json({ message: "Agendamento Completo" });
+    }
+
+    #getPacientesPorDataAgendada(dataAgendada) {
+        if (this.agendamentos.has(dataAgendada)) {
+            return this.agendamentos.get(dataAgendada);
+        }
+        return [];
     }
 
 }
